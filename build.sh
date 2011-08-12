e99dcea6b8e156141dc2c65bb59c8642180497cd
Manfred Moser (mosabua) <manfred@mosabuam.com>
2011-08-12 14:22:22 -0700

    a bit more tuning for build script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf target
 mkdir -p target/images
 mkdir -p target/figs
 
@@ -11,12 +12,13 @@ cd ../..
 # Build the Single HTML Page Version
 asciidoc -o target/book-hudson.html content/book-hudson.doc
 
+
 # Build the PDF
-#rm -rf target/images
-#rm -rf target/figs
-#cp -r figs target
-#cp -r images target
-#a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-nexus.doc
+rm -rf target/images
+rm -rf target/figs
+cp -r content/figs target
+cp -r content/images target
+#a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target content/book-hudson.doc
 
 # Build the Chunked HTML
-#a2x -v -k -fchunked -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target book-nexus.doc
+#a2x -v -k -fchunked -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target content/book-hudson.doc

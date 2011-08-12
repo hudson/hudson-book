061dce53ee965e17bc9a2f1172b6a268986a8abd
Manfred Moser (mosabua) <manfred@mosabuam.com>
2011-08-12 15:06:42 -0700

    some logging on build script and setting error

--- build.sh
+++ build.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
+#fail the script if anything invoked command/script fails
+set -e
+
 rm -rf target
 mkdir -p target/images
 mkdir -p target/figs
 
 # Convert the images
+echo "--- Converting images - Started ---"
 cd content/figs/
 ./convert.sh
 cd ../..
+echo "--- Converting images - Finished ---"
 
 # Build the Single HTML Page Version
+echo "--- Building single HTML page version - Started ---"
 asciidoc -o target/book-hudson.html content/book-hudson.doc
-
+echo "--- Building single HTML page version - Finished ---"
 
 # Build the PDF
 rm -rf target/images
 rm -rf target/figs
 cp -r content/figs target
 cp -r content/images target
+
+echo "--- Building PDF version - Started ---"
 #a2x -v -k -fpdf -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target content/book-hudson.doc
+echo "--- Building PDF version - Finished ---"
 
 # Build the Chunked HTML
+echo "--- Building chunked HTML version - Started ---"
 #a2x -v -k -fchunked -dbook --dblatex-opts=" -P latex.output.revhistory=0" -D target content/book-hudson.doc
+echo "--- Building chunked HTML page version - Finished ---"
+
+echo "--- Build successful --"
